e01c7d2199eed63cbd5cda09b56a99c46e191a06
devexxh <kumarsujal.codes@gmail.com>
2026-01-14 22:14:11 +0530

    backend init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './db.js';
+import dotenv from "dotenv";
+import confessionRouter from './routes/confession.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Ghostboard is alive");
+});
+
+app.use("/confessions", confessionRouter);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
